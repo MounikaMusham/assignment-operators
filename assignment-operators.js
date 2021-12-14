9e03cd7d1c0f39fdbfe534e4621d6c007893714e
mounika <mounikamusham12@gmail.com>
2021-12-14 18:06:55 +0530

    assignment-operators

---
+++ assignment-operators.js
@@ -0,0 +1,45 @@
+
+var num1=20;
+var num2=30;
+var num3=40;
+//-----------------
+
+num1 += 15;
+console.log(num1);
+//---------------
+
+num1 -= 17;
+console.log(num1);
+
+//----------------
+num1 *= 5;
+console.log(num1);
+
+//----------------
+ 
+num1 /= 5;
+console.log(num1);
+
+//------------------
+
+num2 %= 10;
+console.log(num2);
+
+// <<=
+var num4=10;
+num4 <<=10;
+console.log(num1);
+//>>=
+
+var num5=5;
+num5 >>=5;
+console.log(num5);
+//&=
+var num6=10;
+num6 &=5;
+console.log(num6);
+
+//^=
+var num7=15;
+num7 ^=10;
+console.log(num7);
